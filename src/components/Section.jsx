dc51a89bff693981af575cf8795a4cd400136ce9
AntonioDattolo <antonio19794.a@gmail.com>
2024-10-01 12:48:48 +0200

    Add Proxy Props (Training Props)

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,3 +8,17 @@ export default function Section({title, children, id}){
         </section>
     );
 }
+
+// ALTERNATIVA CON LE OBJECT PROPS
+//CON QUESTA SINTASSI, TUTTI GLI ATTRIBUTI AGGIUNTI SUL COMPOMENTE, VENGONO RAGGRUPPATI IN UN OGGETTONE
+//E PASSATTI AL TAG INTERESSATO.
+// export default function Section({title, children, ...props}){
+//     return(
+//         <section {...props}>
+//             <h2>
+//                 {title}
+//             </h2>
+//             {children}
+//         </section>
+//     );
+// }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,13 @@
-export default function TabButton({children, onSelect, isSelected}){
+export default function TabButton({children, isSelected, ...props}){
  
     return (
         <li>
-            <button className={isSelected ? 'active' : ''} onClick={onSelect}>
+            <button className={isSelected ? 'active' : ''} {...props}>
                 {children}
             </button>
         </li>
     )
 }
+
+// con '...props' passiamo l'interazione per cambiare il topic SELEZIONATO --> prima era onclick={onSelect} 
+//cambiando ovviamente in App.jsx da onSelect={()=>handleSelect('xxx')} ==> onClick={()=>handleSelect('xxx')}
